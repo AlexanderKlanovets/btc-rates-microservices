58a7487726ed1db20e5a77287c0c00d5f17f0bff
Alexander Klanovets <alex.klanovets@gmail.com>
2021-09-11 22:52:08 +0300

    New: Add DEBUG_MODE for the auth service

--- auth-service/lib/config.js
+++ auth-service/lib/config.js
@@ -2,8 +2,19 @@
 
 require('dotenv-safe').config();
 
+const parseDebugModeValue = (envVarValue) => {
+  const DEBUG_MODE_VALUES = ['0', '1'];
+
+  if (!DEBUG_MODE_VALUES.includes(envVarValue)) {
+    throw new Error('Invalid debug mode variable value. Should be 0 or 1.');
+  }
+
+  return envVarValue === '1';
+};
+
 module.exports = {
   PORT: process.env.PORT,
   JWT_SECRET: process.env.JWT_SECRET,
   DATA_PATH: process.env.DATA_PATH,
+  DEBUG_MODE: parseDebugModeValue(process.env.DEBUG_MODE),
 };

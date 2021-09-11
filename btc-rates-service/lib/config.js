1159075df4c658019577e81a4dee4fa988311247
Alexander Klanovets <alex.klanovets@gmail.com>
2021-09-11 19:46:02 +0300

    Fix: Add DEBUG_MODE value parsing

--- btc-rates-service/lib/config.js
+++ btc-rates-service/lib/config.js
@@ -2,18 +2,18 @@
 
 require('dotenv-safe').config();
 
-const validateDebugModeValue = (envVarValue) => {
+const parseDebugModeValue = (envVarValue) => {
   const DEBUG_MODE_VALUES = ['0', '1'];
 
   if (!DEBUG_MODE_VALUES.includes(envVarValue)) {
     throw new Error('Invalid debug mode variable value. Should be 0 or 1.');
   }
 
-  return envVarValue;
+  return envVarValue === '1';
 };
 
 module.exports = {
   PORT: process.env.PORT,
   AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL,
-  DEBUG_MODE: validateDebugModeValue(process.env.DEBUG_MODE),
+  DEBUG_MODE: parseDebugModeValue(process.env.DEBUG_MODE),
 };
